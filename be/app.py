545368ac4a4f265c8324d8fc8616c20dab653f44
NickCheng0921 <nickshacheng.0921@gmail.com>
2024-06-01 03:22:41 -0400

    Declarative base and chat length cap

--- be/app.py
+++ be/app.py
@@ -3,24 +3,51 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from datetime import datetime
 from flask_socketio import SocketIO, send
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, func
+
+MAX_ENTRIES = 250
 
 app = Flask(__name__)
 CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-class Message(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(80), nullable=False)
-	content = db.Column(db.String(200), nullable=False)
-	timestamp = db.Column(db.DateTime, default=datetime.now)
+# Define the base for declarative models
+Base = declarative_base()
+
+# Create an instance of the engine
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+
+# Define the Message model using declarative base
+class Message(Base):
+	__tablename__ = 'messages'
+	id = Column(Integer, primary_key=True)
+	username = Column(String(80), nullable=False)
+	content = Column(String(200), nullable=False)
+	timestamp = Column(DateTime, default=datetime.now)
+
+Base.metadata.create_all(engine)
+
+# Set up the session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def count_entries(session):
+	return session.query(func.count(Message.id)).scalar()
+
+def delete_half_entries(session):
+	half_count = count_entries(session) // 2
+	subquery = session.query(Message.id).order_by(Message.timestamp).limit(half_count).subquery()
+	session.query(Message).filter(Message.id.in_(subquery)).delete(synchronize_session=False)
+	session.commit()
 
 @app.route('/messages', methods=['GET'])
 def get_messages():
-	messages = Message.query.order_by(Message.timestamp).all()
+	messages = session.query(Message).order_by(Message.timestamp).all()
 	return jsonify([
 		{
 			'id': msg.id,
@@ -34,8 +61,9 @@ def get_messages():
 def add_message():
 	data = request.get_json()
 	new_message = Message(username=data['username'], content=data['content'])
-	db.session.add(new_message)
-	db.session.commit()
+	session.add(new_message)
+	session.commit()
+
 	return jsonify({'id': new_message.id, 'timestamp': new_message.timestamp.isoformat()}), 201
 
 @socketio.on('connect')
@@ -44,13 +72,20 @@ def handle_connect():
 
 @socketio.on('message')
 def handle_message(msg):
-	if 'username' and 'content' in msg:
+	if 'username' in msg and 'content' in msg:
 		new_message = Message(username=msg['username'], content=msg['content'])
-		db.session.add(new_message)
-		db.session.commit()
+		session.add(new_message)
+		session.commit()
 
-		send({'id': new_message.id, 'timestamp': new_message.timestamp.isoformat(), 'username': msg['username'], 'content': msg['content']}, broadcast=True)
+		if count_entries(session) > MAX_ENTRIES:
+			delete_half_entries(session)
+
+		send({
+			'id': new_message.id,
+			'timestamp': new_message.timestamp.isoformat(),
+			'username': msg['username'],
+			'content': msg['content']
+		}, broadcast=True)
 
 if __name__ == '__main__':
-	db.create_all()
 	socketio.run(app, debug=True)
