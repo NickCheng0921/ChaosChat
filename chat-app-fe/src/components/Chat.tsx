2a2c00d23749e156970e379dcc4668fbe320d8a7
NickCheng0921 <nickshacheng.0921@gmail.com>
2024-06-02 23:59:55 -0700

    Working rearrange messages locally

--- chat-app-fe/src/components/Chat.tsx
+++ chat-app-fe/src/components/Chat.tsx
@@ -22,6 +22,11 @@ const Chat: React.FC = () => {
 	const messagesEndRef = useRef<HTMLDivElement>(null); // Reference to the messages container
 	const [scrollLock, setScrollLock] = useState(true);
 
+	// drag UI
+	const [draggedMsg, setDraggedMsg] = useState<number | null>(null);
+	const [mouse, setMouse] = useState<[number, number]>([0, 0]);
+	const [dropZone, setDropZone] = useState(0);
+
 	// Function to scroll the messages container to the bottom
 	const scrollToBottom = () => {
 		if (scrollLock && messagesEndRef.current) {
@@ -38,6 +43,36 @@ const Chat: React.FC = () => {
 		}
 	}
 
+	//  https://dev.to/h8moss/build-a-reorderable-list-in-react-29on
+	const _reorderListForward = <T,>(l: T[], start: number, end: number) => {
+		const temp = l[start];
+		for (let i=start; i<end; i++) {
+			l[i] = l[i+1];
+		}
+		l[end - 1] = temp;
+
+		return l;
+	};
+
+	const _reorderListBackward = <T,>(l: T[], start: number, end: number) => {
+		const temp = l[start];
+	  
+		for (let i = start; i > end; i--) {
+		  l[i] = l[i - 1];
+		}
+	  
+		l[end] = temp;
+	  
+		return l;
+	  };
+	  
+	const reorderList = <T,>(l: T[], start: number, end: number) => {
+		if (start < end) return _reorderListForward([...l], start, end);
+		else if (start > end) return _reorderListBackward([...l], start, end);
+	  
+		return l; // if start == end
+	};
+
 	useEffect(() => {
 		// get current feed
 		axios.get('http://localhost:5000/messages')
@@ -45,6 +80,45 @@ const Chat: React.FC = () => {
 				setMessages(response.data);
 				scrollToBottom(); // Scroll to bottom initially
 			});
+
+	}, [])
+
+	useEffect(() => {
+		//  mouse movement handler for drag and drop
+		const dragHandler = (e: MouseEvent) => {
+			setMouse([e.x, e.y]);
+		};
+
+		const dropHandler = (e: MouseEvent) => {
+			if (draggedMsg !== null) {
+				e.preventDefault();
+				setDraggedMsg(null);
+
+				setMessages((messages) => reorderList([...messages], draggedMsg, dropZone));
+			}
+		};
+
+		document.addEventListener("mousemove", dragHandler);
+		document.addEventListener("mouseup", dropHandler);
+	
+		// get closest drop zone
+		if (draggedMsg !== null) {
+			// get all drop-zones
+			const elements = Array.from(document.getElementsByClassName("drop-zone"));
+			// get all drop-zones' y-axis position
+			// if we were using a horizontally-scrolling list, we would get the .left property
+			const positions = elements.map((e) => e.getBoundingClientRect().top);
+			// get the difference with the mouse's y position
+			const absDifferences = positions.map((v) => Math.abs(v - mouse[1]));
+	  
+			// get the item closest to the mouse
+			let result = absDifferences.indexOf(Math.min(...absDifferences));
+	  
+			// if the item is below the dragged item, add 1 to the index
+			if (result > draggedMsg) result += 1;
+	  
+			setDropZone(result);
+		}
 
 		//ensure we can receive server side updates
 		socket.on('connect', () => {
@@ -68,10 +142,12 @@ const Chat: React.FC = () => {
 		})
 
 		return () => {
-			socket.off("message")
+			socket.off("message");
+			document.removeEventListener("mousemove", dragHandler);
+			document.removeEventListener("mouseup", dropHandler);
 		};
 
-	}, [scrollLock]);
+	}, [scrollLock, draggedMsg, mouse]);
 
 	const handleSubmit = (e: React.FormEvent) => {
 		e.preventDefault();
@@ -97,10 +173,33 @@ const Chat: React.FC = () => {
 				</div>
 			</div>
 			<div id='messages' ref={messagesEndRef}> {/* Attach ref to the messages container */}
-				{messages.map((msg) => (
-					<div key={msg.id}>
-						<strong>{msg.username.slice(0, 12).padEnd(12, ' ')}</strong>: {msg.content}
-					</div>
+				{draggedMsg !== null && (
+					<div className="floating msg-list-item"
+					style={{
+						left: `${mouse[0]}px`,
+						top: `${mouse[1]}px`,
+					}}>
+					{messages[draggedMsg].content}</div>
+				)}
+
+				<div className={`msg-list-item drop-zone ${draggedMsg === null || dropZone !== 0 ? "hidden" : ""}`}></div>
+				{messages.map((msg, index) => (
+					<>
+						{draggedMsg !== index && (
+							<>
+								<div key={msg.id}
+									className="msg-list-item"
+									onMouseDown={(e) => {
+										e.preventDefault();
+										setDraggedMsg(index);
+									}}>
+									<strong>{msg.username}</strong>: {msg.content}
+								</div>
+								<div className={`msg-list-item drop-zone ${draggedMsg === null || dropZone !== index+1 ? "hidden" : ""}`}></div>
+							</>
+						)}
+
+					</>
 				))}
 			</div>
 			<form onSubmit={handleSubmit}>
@@ -118,7 +217,7 @@ const Chat: React.FC = () => {
 					onChange={(e) => setContent(e.target.value)}
 					required
 				/>
-				<button type="submit">Send</button>
+				<button type="submit" className="hidden">Send</button>
 			</form>
 		</div>
 	);
